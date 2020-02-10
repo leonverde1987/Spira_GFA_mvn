e6f19f35a8093f92e6921c891b13b450ebedbdfa
davidchaveza <david.chavez.avila@gmail.com>
2020-02-10 10:18:27 -0600

    Cambio en id de TC para spira Team

--- src/test/java/SpiraRun/Run.java
+++ src/test/java/SpiraRun/Run.java
@@ -1,7 +1,5 @@
 package SpiraRun;
 
-
-
 import org.junit.Test;
 import com.inflectra.spiratest.addons.junitextension.SpiraTestListener;
 import org.junit.runner.JUnitCore;
@@ -11,9 +9,7 @@ public class Run{
     @Test
     public void TestBuscarGoogle() throws InterruptedException {
         JUnitCore core = new JUnitCore();
-        //Add the custom SpiraTest listener
         core.addListener(new SpiraTestListener());
-        //Finally run the test fixture
         core.run (TestCases.TestOne.class);
     }
 
